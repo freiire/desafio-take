e00b0b4a9319b62a7d37e9939f31fce9d785000e
Leonardo Freire <leonardo.russi@compasso.com.br>
2021-06-29 15:40:45 -0300

    api gateway

---
+++ api/index.js
@@ -0,0 +1,33 @@
+import Express from "express";
+import axios from 'axios'
+const app = Express();
+
+app.get("/", (req, res) => {
+    axios
+      .get("https://api.github.com/users/takenet/repos", {
+          params: {
+              'sort':'created',
+              'direction':'asc'
+          }
+      })
+      .then((response) => {
+        const reposCsharp = [];
+  
+        response.data.filter((repo) => {
+          if (repo["language"] == "C#") {
+            reposCsharp.push({
+              nome: repo["full_name"],
+              descricao: repo.description,
+              data: repo["created_at"],
+              linguagem: repo.language,
+              avatar:repo.owner['avatar_url']
+            });
+          }
+        });
+        return res.json(reposCsharp);
+      })
+      .catch((err) => console.error(err));
+  });
+
+
+app.listen(8000, () => console.log("Running!🚀"));
